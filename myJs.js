781ef94dd85e5c2a8cf075189d8f86ab30ab6db6
rekakovacs97 <74371358+rekakovacs97@users.noreply.github.com>
2021-11-27 00:43:37 +0200

    added welcome alert + person name

--- myJs.js
+++ myJs.js
@@ -3,7 +3,13 @@ document.getElementById("test-button").onclick = function fun()
      var person = prompt("What is your name?");
      console.log (' welcome ' + person);
      alert('Welcome ' + person);
-     document.getElementById("test-button").innerHTML = "KLIKK";
+     document.getElementById("personal-info").style.display = "initial";
+     document.getElementById("personal-info").innerHTML = person;
+    
+
+     //var x = document.getElementById("personal-info");
+
+     //document.getElementById("personal-info").innerHTML = person;
     }   
 
-   
+ 
