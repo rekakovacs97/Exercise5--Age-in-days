175af19adc5bde826a9fc49c89f6f0f3503dcbb3
rekakovacs97 <74371358+rekakovacs97@users.noreply.github.com>
2021-12-23 16:30:24 +0200

    editing, editing

--- myJs.js
+++ myJs.js
@@ -4,6 +4,5 @@ document.getElementById("test-button").onclick = function fun()
      console.log (' welcome ' + person);
      alert('Welcome ' + person);
      document.getElementById("personal-info").style.display = "initial";
-    console.log(person);
     }   
 
